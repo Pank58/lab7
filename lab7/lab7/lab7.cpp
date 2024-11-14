d0e5cc2b2aee618602a83d7db8ab61b2fb3437b7
PichaevIvan <ivan.pichaev@mail.ru>
2024-11-14 22:18:21 +0300

    add task 2

--- lab7/lab7/lab7.cpp
+++ lab7/lab7/lab7.cpp
@@ -1,121 +1,80 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
 #include <locale.h>
 
-
-typedef struct Node {
-    int vertex;
-    struct Node* next;
-} Node;
-
-
-
-void createAdjacencyList(Node** adjList, int N) {
+void createG(int** G, int N) {
     for (int i = 0; i < N; i++) {
         for (int j = i + 1; j < N; j++) {
             int edge = rand() % 2;
-            if (edge == 1) {
-                Node* newNode = (Node*)malloc(sizeof(Node));
-                newNode->vertex = j;
-                newNode->next = adjList[i];
-                adjList[i] = newNode;
-
-                newNode = (Node*)malloc(sizeof(Node));
-                newNode->vertex = i;
-                newNode->next = adjList[j];
-                adjList[j] = newNode;
-            }
+            G[i][j] = edge;
+            G[j][i] = edge;
         }
+    }
+
+    for (int i = 0; i < N; i++) {
+        G[i][i] = 0;
     }
 }
 
-int compare(const void* a, const void* b) {
-    return (*(int*)a - *(int*)b);
-}
-
-void sortAdjacencyList(Node** adjList, int N) {
+void printG(int** G, int N) {
+    printf("Матрица смежности:\n");
     for (int i = 0; i < N; i++) {
-        int count = 0;
-        Node* temp = adjList[i];
-        while (temp != NULL) {
-            count++;
-            temp = temp->next;
-        }
-
-        int* vertices = (int*)malloc(count * sizeof(int));
-        temp = adjList[i];
-        int index = 0;
-        while (temp != NULL) {
-            vertices[index++] = temp->vertex;
-            temp = temp->next;
-        }
-
-        qsort(vertices, count, sizeof(int), compare);
-
-        temp = adjList[i];
-        index = 0;
-        while (temp != NULL) {
-            temp->vertex = vertices[index++];
-            temp = temp->next;
-        }
-
-        free(vertices);
-    }
-}
-
-void printAdjacencyList(Node** adjList, int N) {
-    printf("Список смежности:\n");
-    for (int i = 0; i < N; i++) {
-        printf("%d: ", i + 1);
-        Node* temp = adjList[i];
-        while (temp != NULL) {
-            printf("%d -> ", temp->vertex + 1);
-            temp = temp->next;
+        for (int j = 0; j < N; j++) {
+            printf("%d ", G[i][j]);
         }
         printf("\n");
     }
 }
 
-void DFS(Node** adjList, int N, int* NUM, int v) {
-    NUM[v] = 1;
-    printf("%d ", v + 1);
+void DFS(int** G, int N, int* NUM, int v) {
+    int* stack = (int*)malloc(N * sizeof(int));
+    int top = -1;
 
-    Node* temp = adjList[v];
-    while (temp != NULL) {
-        if (NUM[temp->vertex] == 0) {
-            DFS(adjList, N, NUM, temp->vertex);
+    stack[++top] = v;
+
+
+
+    while (top != -1) {
+        int vec = stack[top--];
+        if (NUM[vec] == 0) {
+            NUM[vec] = 1;
+            printf("%d ", vec + 1);
         }
-        temp = temp->next;
+        for (int i = N - 1; i >= 0; i--) {
+            if (G[vec][i] == 1 && NUM[i] == 0) {
+                stack[++top] = i;
+            }
+        }
     }
 }
 
 int main() {
+
     setlocale(LC_ALL, "");
     int N;
 
     printf("Введите количество вершин в графе: ");
     scanf("%d", &N);
-
-    Node** adjList = (Node**)malloc(N * sizeof(Node*));
+    int** G = (int**)malloc(N * sizeof(int*));
     for (int i = 0; i < N; i++) {
-        adjList[i] = NULL;
+        G[i] = (int*)malloc(N * sizeof(int));
     }
-
-    createAdjacencyList(adjList, N);
-    sortAdjacencyList(adjList, N);
-    printAdjacencyList(adjList, N);
+    createG(G, N);
+    printG(G, N);
 
     int* NUM = (int*)malloc(N * sizeof(int));
     for (int i = 0; i < N; i++) {
         NUM[i] = 0;
     }
+
     printf("\nРезультат обхода в глубину:\n");
     for (int i = 0; i < N; i++) {
         if (NUM[i] == 0) {
-            DFS(adjList, N, NUM, i);
+            DFS(G, N, NUM, i);
         }
     }
 }
